8fd9b0ffdd836437c1d5e9a6219c99f891177f60
LEANDER KAMMERMEIER <Leander.kammermeier@student.htw-berlin.de>
2021-06-03 14:26:25 +0200

    fixed functionality

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,26 +1,36 @@
 
-public class Postfix {
+public class Postfix{
 
+	public static void main(String[] args) {
+		
+		Postfix p = new Postfix();
+		p.evaluate("35+");
+	}
+	
 	public void evaluate (String pfx) {
-	 
+	    StackAsList<Character> stack = new StackAsList<Character>();
 
 		String r = "";
 		char t = ' ';
 		
-		while(pfx != "") {
+		//while(pfx != "") {
 			
 		for( int i = 0; i < pfx.length(); i++) {
 			
 			t = pfx.charAt(i);
-			if(t == 1 || t == 2 || t == 3 || t == 4 || t == 5 || t == 6 || t == 7 || t == 8 || t == 9) {
-				push(t);
+			
+			
+			if(t == '1' || t == '2' || t == '3' || t == '4' || t == '5' || t == '6' || t == '7' || t == '8' || t == '9') {
+				stack.push(t);
 			}
 			
 			if(t == '*' || t == '+' || t == '-' || t=='/') {
-			   int rhs = Character.getNumericValue(top());
-			   pop();
-			   int lhs = Character.getNumericValue(top());
-			   pop();
+			   int rhs = Character.getNumericValue(stack.top());
+			   stack.pop();
+			   
+			   int lhs = Character.getNumericValue(stack.top());
+			   stack.pop();
+			  
 			   
 			   int a = 0;
 			   if(t == '*') {
@@ -35,16 +45,14 @@ public class Postfix {
 			   if(t == '/') {
 				   a = lhs / rhs;
 			   }
-			   push((char) a);
+			   stack.push((char) (a + '0'));
 			   
 			}
-			System.out.println("result is:" + top());
-			
-			
 			
 		}
+		System.out.println("result is:" + Character.getNumericValue(stack.top()));
 		
-	}
+	//}
 		
 	}
 }
