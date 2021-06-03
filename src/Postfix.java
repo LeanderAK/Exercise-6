2625c5cc8e5ffcd57ed3eb4fa8a0103a6f4b72a1
LEANDER KAMMERMEIER <Leander.kammermeier@student.htw-berlin.de>
2021-06-03 13:38:32 +0200

    add Postfix.java

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,4 +1,50 @@
 
 public class Postfix {
 
+	public void evaluate (String pfx) {
+	 
+
+		String r = "";
+		char t = ' ';
+		
+		while(pfx != "") {
+			
+		for( int i = 0; i < pfx.length(); i++) {
+			
+			t = pfx.charAt(i);
+			if(t == 1 || t == 2 || t == 3 || t == 4 || t == 5 || t == 6 || t == 7 || t == 8 || t == 9) {
+				push(t);
+			}
+			
+			if(t == '*' || t == '+' || t == '-' || t=='/') {
+			   int rhs = Character.getNumericValue(top());
+			   pop();
+			   int lhs = Character.getNumericValue(top());
+			   pop();
+			   
+			   int a = 0;
+			   if(t == '*') {
+				   a = lhs * rhs;
+			   }
+			   if(t == '+') {
+				   a = lhs + rhs;
+			   }
+			   if(t == '-') {
+				   a = lhs - rhs;
+			   }
+			   if(t == '/') {
+				   a = lhs / rhs;
+			   }
+			   push((char) a);
+			   
+			}
+			System.out.println("result is:" + top());
+			
+			
+			
+		}
+		
+	}
+		
+	}
 }
