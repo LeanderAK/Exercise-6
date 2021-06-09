473cf3d1e52d35881c691abbbc105e121388c5fd
Jonas Blome <jonas.blome@gmx.de>
2021-06-09 22:38:58 +0200

    infix to postfix now fixed (lol)

--- src/Postfix.java
+++ src/Postfix.java
@@ -7,8 +7,7 @@ public class Postfix{
 		Postfix p = new Postfix();
 		p.evaluate("35+");
 		Scanner scanner = new Scanner( System.in );
-		String s = scanner.next();
-	    System.out.println("Input string is:" + s);
+		String s = scanner.nextLine();
 		System.out.println(p.infixToPostfix(s));
 		scanner.close();
 	}
@@ -16,43 +15,36 @@ public class Postfix{
 	
 	public String infixToPostfix (String ifx) {
 	  StackAsList<Character> stack = new StackAsList<Character>();
-	  String r = "";
-	  char t = ' ';
-	  int i = 0;
-	  while(i < ifx.length()) {
-		  t = ifx.charAt(i);
-		  if(t == '0' || t == '1' || t == '2' || t == '3' || t == '4' || t == '5' || t == '6' || t == '7' || t == '8' || t == '9') {
-			  r = r + t;
+	  String result = "";
+	  for(int i = 0; i < ifx.length(); i++) {
+		  char t = ifx.charAt(i);
+		  if (t == '0' || t == '1' || t == '2' || t == '3' || t == '4' || t == '5' || t == '6' || t == '7' || t == '8' || t == '9') {
+			  result += t;
 		  }
-		  else if(t == '(') {
+		  else if (t == '(') {
 			  stack.push(t);
 		  }
-		  else if(t == ')') {
-			  while(stack.top() != '(') {
-				  r = r + stack.top();
-				  stack.pop();
+		  else if (t == ')') {
+			  while(stack.top() != null && stack.top() != '(') {
+				  result += stack.pop();
 			  }
 			  stack.pop();
 		  }
-		  else if(t == '-' || t == '+' || t == '/' || t == '*' || t == '%' ) {
-			  
-			  while(precedence(stack.top()) < precedence(t)) {
-				  r = r + stack.top();
-				  System.out.println(stack.pop());
-				  if(stack.top() == null) {
-					  break;
-				  }
+		  else {
+			  while(stack.top() != null && precedence(t) < precedence(stack.top())) {
+				  result += stack.pop();
 			  }
-			 
 			  stack.push(t);
 		  }
-		  while(stack.top() != null) {
-			  r = r + stack.top();
-			  stack.pop();
-		  }
-		  i++;
 	  }
-      return r;
+	  
+	  while(stack.top() != null) {
+		  if (stack.top() == '(') {
+			  return "Invalid Expression";
+		  }
+		  result += stack.pop();
+	  }
+      return result;
 	}
 	
 	public int precedence(char t) {
@@ -63,17 +55,14 @@ public class Postfix{
 		if(t == '*' || t == '/' || t == '%') {
 			return 2;
 		}
-		return 0;
+		return -1;
 	}
-
 	
 	public void evaluate (String pfx) {
 	    StackAsList<Character> stack = new StackAsList<Character>();
 
-		String r = "";
+		String result = "";
 		char t = ' ';
-		
-		//while(pfx != "") {
 			
 		for( int i = 0; i < pfx.length(); i++) {
 			
@@ -90,7 +79,6 @@ public class Postfix{
 			   
 			   int lhs = Character.getNumericValue(stack.top());
 			   stack.pop();
-			  
 			   
 			   int a = 0;
 			   if(t == '*') {
@@ -105,8 +93,10 @@ public class Postfix{
 			   if(t == '/') {
 				   a = lhs / rhs;
 			   }
+			   if(t == '%') {
+				   a = lhs % rhs;
+			   }
 			   stack.push((char) (a + '0'));
-			   
 			}
 			
 		}
--- src/Stack.java
+++ src/Stack.java
@@ -1,12 +1,8 @@
 
 public interface Stack<T> {
-
 	abstract T pop();
 	
 	abstract T top();
 	
     abstract void push(T elem);
-    
-		
-	
 }
